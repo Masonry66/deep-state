210ee765b4bd1963b1d56c722c79374f3ba5f762
Masonry66 <tccmcorp@gmail.com>
2025-07-29 20:17:00 -0400

    Footer.tsx

---
+++ deepstat folder/Footer.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="w-full py-4 mt-8">
+      <p className="text-center text-xs text-gray-500">
+        powered by mexcorp 2.5
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
